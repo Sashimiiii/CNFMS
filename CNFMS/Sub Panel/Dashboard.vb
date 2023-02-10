34a3dc0ba38fa6496c1955d5c696f76ef1a51f49
Sashimiiii <jelynelle.bastasa@gmail.com>
2023-02-10 22:01:34 +0800

    Delete Comment

--- CNFMS/Sub Panel/Dashboard.vb
+++ CNFMS/Sub Panel/Dashboard.vb
@@ -334,7 +334,6 @@ Public Class Dashboard
 
     Private Sub Dashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         showFuelInfo()
-        ' showGraph()
         showMonthSale()
         showDailySale()
         nameDisplay.Text = Nameuser
